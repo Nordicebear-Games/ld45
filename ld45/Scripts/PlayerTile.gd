8272f87cfeacf00e0ee237753463b545681408f9
avodhel <uyelik-eposta@hotmail.com>
2019-10-05 17:06:45 +0300

    now player tile can move up and down too

--- ld45/Scripts/PlayerTile.gd
+++ ld45/Scripts/PlayerTile.gd
@@ -9,10 +9,13 @@ func _physics_process(delta):
 func move():
 	# Calculate the direction the player is trying to go
 	var dir = Vector2(0, 0)
-	
-	if (Input.is_action_just_pressed("ui_right") && grid_x < grid_width - 1):
+	if (Input.is_action_just_pressed("ui_up") && grid_y > 0):
+		dir = Vector2(0, -1)
+	elif (Input.is_action_just_pressed("ui_down") && grid_y < grid_height - 1):
+		dir = Vector2(0, 1)
+	elif (Input.is_action_just_pressed("ui_right") && grid_x < grid_width - 1):
 		dir = Vector2(1, 0)
-	if (Input.is_action_just_pressed("ui_left") && grid_x > 0):
+	elif (Input.is_action_just_pressed("ui_left") && grid_x > 0):
 		dir = Vector2(-1, 0)
 
 	# Move the player to the new position
@@ -21,6 +24,6 @@ func move():
 		self.position = grid_to_pixel(target[0], target[1])
 		grid_x = target[0]
 		grid_y = target[1]
-	
+		print(str(grid_x) + " " + str(grid_y))
 	# Set direction back to nothing
 	dir = Vector2(0, 0)

9d0b7b0649d5896d593a46a53d5957b9f15a499e
avodhel <uyelik-eposta@hotmail.com>
2019-10-07 22:28:28 +0300

    tutorial - 4

--- ld45/Tutorial/Tutorial.gd
+++ ld45/Tutorial/Tutorial.gd
@@ -8,18 +8,18 @@ var dialog = [
 	"Move your aim with left and right arrow keys and grab the tile which coming towards you.",
 	"Tip: Choose a high numbered tile.",
 	"You have your own tile now.", 
-	"The number of the center means your move count.", 
-	"After every move, move count'll reduce.",
-	"if move count is 0 you can't move.",
-	"Piggy bank give you extra points to use later. Grab it if you see one.",
-	"You can gain extra life. If you grab a life tile. Grab it if you see one.",
-	"You grabbed piggy bank tile, press 'S' to use it. Tip: Use it wisely.",
-	"Piggy bank used and move count increased now.",
-	"You grabbed a life tile and this gave you an extra life.",
-	"You grabbed same colored point tile. Your move count increased!",
-	"You grabbed wrong colored point tile. Your move count reduced!",
-	"Color changed. Tip: Color changes every 30 seconds.",
-	"Bomb exploded. Tip:Stay away from bombs."]
+	"The number of the center means your 'move count'.", 
+	"After every move, 'move count'll reduce.",
+	"if 'move count' is '0' you can't move.",
+	"Piggy bank give you extra points to use later.",
+	"You can gain extra life. If you grab a life tile.",
+	"You grabbed 'piggy bank tile', press 'S' to use it. Tip: Use it wisely.",
+	"Piggy bank used and 'move count' increased now.",
+	"You grabbed a 'life tile' and this gave you an 'extra' life.",
+	"You grabbed same colored point tile. Your 'move count' increased!",
+	"You grabbed wrong colored point tile. Your 'move count' reduced!",
+	"Color changed. Tip: Color changes every '30' seconds.",
+	"Bomb exploded. Tip: Stay away from bombs."]
 var page = 0
 
 # Functions
